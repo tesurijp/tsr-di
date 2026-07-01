1aef570fad0ddb54eb688709e78a70198e0d82fd
tesuri <tesuri@gmail.com>
2026-07-01 22:49:50 +0900

    関数解決関連のテスト追加

--- tsr-di.test/DelegateTest.cs
+++ tsr-di.test/DelegateTest.cs
@@ -12,18 +12,146 @@ public sealed class DelegateTest
     [TestMethod]
     public void NonRegisterdClassFunctions()
     {
-        var f1 = ServiceResolver.Resolve<IGetParentName>();
-        var f2 = ServiceResolver.Resolve<PreDefineIntFunction>();
-        var f3 = ServiceResolver.Resolve<PreDefineVoidFunction>();
-        var f4 = ServiceResolver.Resolve<ILastResult>();
-        var f2b = f2.Bind(10);
-        var f3b = f3.Bind(10, 11);
-        Assert.AreEqual("NonRegisterdClass static", f1());
-        Assert.AreEqual((1 + 2 + 3).ToString(), f2(1, 2, 3));
-        Assert.AreEqual((10 + 2 + 3).ToString(), f2b(2, 3));
-        f3(3, 4, 5);
-        Assert.AreEqual((3 + 4 + 5), f4());
-        f3b(3);
-        Assert.AreEqual((10 + 11 + 3), f4());
+        const string StrResult = "NonRegisterClass";
+        var f1 = ServiceResolver.Resolve<IGetClassNameNonRegisterd>();
+        var f2 = ServiceResolver.Resolve<IGetClassName1>(ServiceKey.NonRegister);
+        var f3 = ServiceResolver.Resolve<IGetParentNameNonRegister>();
+        var f4 = ServiceResolver.Resolve<IGetParentName>(ServiceKey.NonRegister);
+        var f5 = ServiceResolver.Resolve<PreDefineVoidFunction>(ServiceKey.NonRegister);
+        var f6 = ServiceResolver.Resolve<ILastResult>(ServiceKey.NonRegister);
+        var f7 = ServiceResolver.Resolve<ILastResult2>(ServiceKey.NonRegister2);
+        var f8 = ServiceResolver.Resolve<PreDefineIntFunction>(ServiceKey.NonRegister);
+        var f9 = ServiceResolver.Resolve<ILastResult>(ServiceKey.NonRegister);
+        var f5o = f5.Bind(1);
+        var f8o = f8.Bind(1, 2);
+        Assert.AreEqual(StrResult, f1());
+        Assert.AreEqual(StrResult, f2());
+        Assert.AreEqual(StrResult, f3());
+        Assert.AreEqual(StrResult, f4());
+        Assert.AreEqual(0, f6());
+        f5o(1, 2);
+        Assert.AreEqual((1+1+2)*2, f7());
+        Assert.AreEqual(2+3+3, f8(2,3,3));
+        Assert.AreEqual(1+2+8, f8o(8));
+        Assert.AreEqual(1+1+2, f9());
+    }
+
+    [TestMethod]
+    public void SingletonClassFunctions()
+    {
+        const string StrResult = "SingletonClass";
+        var f1 = ServiceResolver.Resolve<IGetClassNameSingletond>();
+        var f2 = ServiceResolver.Resolve<IGetClassName1>(ServiceKey.Singleton);
+        var f3 = ServiceResolver.Resolve<IGetParentNameSingleton>();
+        var f4 = ServiceResolver.Resolve<IGetParentName>(ServiceKey.Singleton);
+        var f5 = ServiceResolver.Resolve<PreDefineVoidFunction>(ServiceKey.Singleton);
+        var f6 = ServiceResolver.Resolve<ILastResult>(ServiceKey.Singleton);
+        var f7 = ServiceResolver.Resolve<ILastResult2>(ServiceKey.Singleton2);
+        var f8 = ServiceResolver.Resolve<PreDefineIntFunction>(ServiceKey.Singleton);
+        var f9 = ServiceResolver.Resolve<ILastResult>(ServiceKey.Singleton);
+        var f5o = f5.Bind(1);
+        var f8o = f8.Bind(1, 2);
+        Assert.AreEqual(StrResult, f1());
+        Assert.AreEqual(StrResult, f2());
+        Assert.AreEqual(StrResult, f3());
+        Assert.AreEqual(StrResult, f4());
+        Assert.AreEqual(0, f6());
+        f5o(1, 2);
+        Assert.AreEqual((1+1+2-2)*2, f7());
+        Assert.AreEqual(2+3+3-1, f8(2,3,3));
+        Assert.AreEqual(1+2+8-1, f8o(8));
+        Assert.AreEqual(1+1+2-2, f9());
+    }
+
+    [TestMethod]
+    public void TransientClassFunctions()
+    {
+        const string StrResult = "TransientClass";
+        var f1 = ServiceResolver.Resolve<IGetClassNameTransientd>();
+        var f2 = ServiceResolver.Resolve<IGetClassName1>(ServiceKey.Transient);
+        var f3 = ServiceResolver.Resolve<IGetParentNameTransient>();
+        var f4 = ServiceResolver.Resolve<IGetParentName>(ServiceKey.Transient);
+        var f5 = ServiceResolver.Resolve<PreDefineVoidFunction>(ServiceKey.Transient);
+        var f6 = ServiceResolver.Resolve<ILastResult>(ServiceKey.Transient);
+        var f7 = ServiceResolver.Resolve<ILastResult2>(ServiceKey.Transient2);
+        var f8 = ServiceResolver.Resolve<PreDefineIntFunction>(ServiceKey.Transient);
+        var f9 = ServiceResolver.Resolve<ILastResult>(ServiceKey.Transient);
+
+        var (f10, f11) = ServiceResolver.Resolve<PreDefineVoidFunction, ILastResult>(ServiceKey.Transient, ServiceKey.Transient);
+        var (f12, f13, f14) = ServiceResolver.Resolve<PreDefineVoidFunction, ILastResult, ILastResult2>(ServiceKey.Transient, ServiceKey.Transient, ServiceKey.Transient2);
+
+        var f5o = f5.Bind(1);
+        var f8o = f8.Bind(1, 2);
+        Assert.AreEqual(StrResult, f1());
+        Assert.AreEqual(StrResult, f2());
+        Assert.AreEqual(StrResult, f3());
+        Assert.AreEqual(StrResult, f4());
+        Assert.AreEqual(0, f6());
+        f5o(1, 2);
+        Assert.AreEqual(0, f7());
+        Assert.AreEqual(2+3+3-1, f8(2,3,3));
+        Assert.AreEqual(1+2+8-1, f8o(8));
+        Assert.AreEqual(0, f9());
+
+        f10(4, 5, 6);
+        Assert.AreEqual(0, f11());
+        Assert.AreEqual(0, f7());
+        f12(5, 6, 7);
+        Assert.AreEqual(0, f13());
+        Assert.AreEqual(0, f14());
+        Assert.AreEqual(0, f11());
+        Assert.AreEqual(0, f7());
+    }
+
+    [TestMethod]
+    public void ScopedClassFunctions()
+    {
+        const string StrResult = "ScopedClass";
+        var f1 = ServiceResolver.Resolve<IGetClassNameScoped>();
+        var f2 = ServiceResolver.Resolve<IGetClassName1>(ServiceKey.Scoped);
+        var f3 = ServiceResolver.Resolve<IGetParentNameScoped>();
+        var f4 = ServiceResolver.Resolve<IGetParentName>(ServiceKey.Scoped);
+        var f5 = ServiceResolver.Resolve<PreDefineVoidFunction>(ServiceKey.Scoped);
+        var f6 = ServiceResolver.Resolve<ILastResult>(ServiceKey.Scoped);
+        var f7 = ServiceResolver.Resolve<ILastResult2>(ServiceKey.Scoped2);
+        var f8 = ServiceResolver.Resolve<PreDefineIntFunction>(ServiceKey.Scoped);
+        var f9 = ServiceResolver.Resolve<ILastResult>(ServiceKey.Scoped);
+
+        var (f10, f11) = ServiceResolver.Resolve<PreDefineVoidFunction, ILastResult>(ServiceKey.Scoped, ServiceKey.Scoped);
+        var (f12, f13, f14) = ServiceResolver.Resolve<PreDefineVoidFunction, ILastResult, ILastResult2>(ServiceKey.Scoped, ServiceKey.Scoped, ServiceKey.Scoped2);
+
+        var f5o = f5.Bind(1);
+        var f8o = f8.Bind(1, 2);
+        Assert.AreEqual(StrResult, f1());
+        Assert.AreEqual(StrResult, f2());
+        Assert.AreEqual(StrResult, f3());
+        Assert.AreEqual(StrResult, f4());
+        Assert.AreEqual(0, f6());
+        f5o(1, 2);
+        Assert.AreEqual(0, f7());
+        Assert.AreEqual(2+3+3-1, f8(2,3,3));
+        Assert.AreEqual(1+2+8-1, f8o(8));
+        Assert.AreEqual(0, f9());
+        f10(4, 5, 6);
+        Assert.AreEqual(4+5+6-2, f11());
+        Assert.AreEqual(0, f7());
+        f12(5, 6, 7);
+        Assert.AreEqual(5+6+7-2, f13());
+        Assert.AreEqual((5+6+7-2)*2, f14());
+        Assert.AreEqual(4+5+6-2, f11());
+        Assert.AreEqual(0, f7());
+    }
+
+    [TestMethod]
+    public void ComplexFunction()
+    {
+        var (f1, f2) = ServiceResolver.Resolve<PreDefineVoidFunction, ILastResult>(ServiceKey.Scoped, ServiceKey.Transient);
+        var (f3, f4, f5) = ServiceResolver.Resolve<PreDefineVoidFunction, ILastResult, ILastResult2>(ServiceKey.Scoped, ServiceKey.Transient, ServiceKey.Scoped2);
+        f1(4, 5, 6);
+        Assert.AreEqual(0, f2());
+        f3(5, 6, 7);
+        Assert.AreEqual(0, f4());
+        Assert.AreEqual((5+6+7-2)*2, f5());
+        Assert.AreEqual(0, f2());
     }
 }
--- tsr-di.test/_ServiceFunctions.cs
+++ tsr-di.test/_ServiceFunctions.cs
@@ -3,20 +3,127 @@
 public interface IFunctionPlaceHolder;
 
 public delegate void PreDefineVoidFunction(int x, int y, int z);
-public delegate string PreDefineIntFunction(int x,int y, int z);
+public delegate int PreDefineIntFunction(int x,int y, int z);
 
 public class NonRegisterdClass
 {
+    private static string GetClassName() => "NonRegisterClass";
+
     [ServiceFunction]
-    public static string GetParentName() => "NonRegisterdClass static";
+    public static string GetClassNameNonRegisterd() => GetClassName();
 
-    [ServiceFunction(ServiceType = typeof(PreDefineIntFunction))]
-    public static string GetParentName(int x, int y, int z) => (x + y + z).ToString();
+    [ServiceFunction(Name="NonRegister")]
+    public static string GetClassName1() => GetClassName();
 
-    static int lastResult = 0;
-    [ServiceFunction(ServiceType = typeof(PreDefineVoidFunction))]
-    public static void VoidFunc(int x, int y, int z) => lastResult = x + y + z;
+    [ServiceFunction(ServiceName="GetParentNameNonRegister")]
+    public static string GetClassName2() => GetClassName();
 
-    [ServiceFunction(ServiceName = "LastResult")]
-    public static int VoidFuncResult() => lastResult;
+    [ServiceFunction(ServiceName="GetParentName", Name="NonRegister")]
+    public static string GetClassName3() => GetClassName();
+
+    private static int lastResult = 0;
+    [ServiceFunction(ServiceType=typeof(PreDefineVoidFunction), Name="NonRegister")]
+    public static void Func1(int x, int y, int z) => lastResult = x+y+z;
+
+    [ServiceFunction(Name="NonRegister")]
+    public static int LastResult() => lastResult;
+
+    [ServiceFunction(Name="NonRegister2")]
+    public static int LastResult2() => lastResult * 2;
+
+    [ServiceFunction(ServiceType=typeof(PreDefineIntFunction), Name="NonRegister")]
+    public static int Func2(int x, int y, int z) => x+y+z;
+
+}
+[ServiceClass(LifeTime=LifeTime.Singleton, Name ="Singleton")]
+public class RegisterdClassSingleton : IFunctionPlaceHolder
+{
+    private string GetClassName() => "SingletonClass";
+
+    [ServiceFunction]
+    public string GetClassNameSingletond() => GetClassName();
+
+    [ServiceFunction(Name="Singleton")]
+    public string GetClassName1() => GetClassName();
+
+    [ServiceFunction(ServiceName="GetParentNameSingleton")]
+    public string GetClassName2() => GetClassName();
+
+    [ServiceFunction(ServiceName="GetParentName", Name="Singleton")]
+    public string GetClassName3() => GetClassName();
+
+    private int lastResult = 0;
+    [ServiceFunction(ServiceType=typeof(PreDefineVoidFunction), Name="Singleton")]
+    public void Func1(int x, int y, int z) => lastResult = x+y+z-2;
+
+    [ServiceFunction(Name="Singleton")]
+    public int LastResult() => lastResult;
+
+    [ServiceFunction(Name="Singleton2")]
+    public int LastResult2() => lastResult * 2;
+
+    [ServiceFunction(ServiceType=typeof(PreDefineIntFunction), Name="Singleton")]
+    public int Func2(int x, int y, int z) => x+y+z-1;
+
+}
+[ServiceClass(LifeTime=LifeTime.Transient,Name ="Transient")]
+public class RegisterdClassTransient : IFunctionPlaceHolder
+{
+    private string GetClassName() => "TransientClass";
+
+    [ServiceFunction]
+    public string GetClassNameTransientd() => GetClassName();
+
+    [ServiceFunction(Name="Transient")]
+    public string GetClassName1() => GetClassName();
+
+    [ServiceFunction(ServiceName="GetParentNameTransient")]
+    public string GetClassName2() => GetClassName();
+
+    [ServiceFunction(ServiceName="GetParentName", Name="Transient")]
+    public string GetClassName3() => GetClassName();
+
+    private int lastResult = 0;
+    [ServiceFunction(ServiceType=typeof(PreDefineVoidFunction), Name="Transient")]
+    public void Func1(int x, int y, int z) => lastResult = x+y+z-2;
+
+    [ServiceFunction(Name="Transient")]
+    public int LastResult() => lastResult;
+
+    [ServiceFunction(Name="Transient2")]
+    public int LastResult2() => lastResult * 2;
+
+    [ServiceFunction(ServiceType=typeof(PreDefineIntFunction), Name="Transient")]
+    public int Func2(int x, int y, int z) => x+y+z-1;
+}
+[ServiceClass(LifeTime=LifeTime.Scoped, Name ="Scoped")]
+public class RegisterdClassScoped : IFunctionPlaceHolder
+{
+    private string GetClassName() => "ScopedClass";
+
+    [ServiceFunction]
+    public string GetClassNameScoped() => GetClassName();
+
+    [ServiceFunction(Name="Scoped")]
+    public string GetClassName1() => GetClassName();
+
+    [ServiceFunction(ServiceName="GetParentNameScoped")]
+    public string GetClassName2() => GetClassName();
+
+    [ServiceFunction(ServiceName="GetParentName", Name="Scoped")]
+    public string GetClassName3() => GetClassName();
+
+    private int lastResult = 0;
+    [ServiceFunction(ServiceType=typeof(PreDefineVoidFunction), Name="Scoped")]
+    public void Func1(int x, int y, int z) => lastResult = x+y+z-2;
+
+    [ServiceFunction(Name="Scoped")]
+    public int LastResult() => lastResult;
+
+    [ServiceFunction(Name="Scoped2")]
+    public int LastResult2() => lastResult * 2;
+
+    [ServiceFunction(ServiceType=typeof(PreDefineIntFunction), Name="Scoped")]
+    public int Func2(int x, int y, int z) => x+y+z-1;
+
 }
